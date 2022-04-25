69509c101201a1babd5cef8cb9a0844c943869a4
lxb007981 <lxb007981@hotmail.com>
2022-04-25 13:13:46 +0000

    Deploying to gh-pages from @ lxb007981/fyp-frontend-elementui@04c069038f1e82f3b5fbef6450dead9ca2145cdd 🚀

---
+++ assets/CrowdCounting.947bac7a.js
@@ -0,0 +1,1 @@
+import{_ as s,o as t,c as _,s as c,t as a,a as o}from"./index.193ae9e2.js";const d={},n=e=>(c("data-v-699d1449"),e=e(),a(),e),r={class:"bgimg"},i=n(()=>o("div",{class:"middle"},[o("hr"),o("h1",null,"Coming Soon!"),o("hr")],-1)),p=[i];function l(e,u){return t(),_("div",r,p)}var v=s(d,[["render",l],["__scopeId","data-v-699d1449"]]);export{v as default};
